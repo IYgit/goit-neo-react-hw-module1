09599884b8b3b687495a509a8f6d003af753ddd6
i.yanchyk <i.yanchyk@steel.mil.ua>
2024-11-18 13:41:59 +0200

    deliting React import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Profile from './components/Profile/Profile';
 import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
